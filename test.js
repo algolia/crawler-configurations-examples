d944ac6e8be7f0e9db81e520625db5739c74db15
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-12-06 14:34:19 +0100

    add first test: "has a record that contains phrase from paragraph"

--- test.js
+++ test.js
@@ -1,5 +1,35 @@
 const { URL } = require('url');
 const cheerio = require('cheerio');
+const junit = require('junit');
+
+function loadRecordExtractor(configFile) {
+  const { startUrls, actions } = require(`./${configFile}`);
+  const { recordExtractor } = actions[0];
+  return ({ url = new URL(startUrls[0]), title = 'Some page', html = '' }) =>
+    recordExtractor({
+      url,
+      title,
+      $: cheerio.load(html),
+    });
+}
+
+function test(recordExtractor) {
+  const paragPhrase = 'Hello World!';
+  const records = recordExtractor({
+    html: `<html><body><p>${paragPhrase}</p></body></html>`,
+  });
+
+  const it = junit();
+  it(`has a record that contains phrase from paragraph`, () =>
+    it.eq(
+      records.some(({ text }) => text.includes(paragPhrase)),
+      true
+    ));
+
+  return it.run();
+}
+
+// start of actual script
 
 const configFile = process.argv[2];
 
@@ -8,14 +38,4 @@ if (!configFile) {
   process.exit(1);
 }
 
-const { startUrls, actions } = require(`./${configFile}`);
-
-const { recordExtractor } = actions[0];
-
-const records = recordExtractor({
-  url: new URL(startUrls[0]),
-  title: 'Some page',
-  $: cheerio.load('<html><body><p>Hello World!</p></body></html>'),
-});
-
-console.log('=>', { records });
+test(loadRecordExtractor(configFile));
